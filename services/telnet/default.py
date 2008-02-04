96544793123aed904603e0183f02b03ba340fce3
Robert Barsch <mail@robert-barsch.de>
2008-02-04 01:20:36 +0000

    - telnet services enable/disable feature

--- services/telnet/default.py
+++ services/telnet/default.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from twisted.application import service
+from twisted.internet import defer
 
 from seishub.core import Component, implements
 from seishub.services.telnet.interfaces import ITelnetCmd
 
+
+ENABLE = ['start','enable', 'on']
+DISABLE = ['stop', 'disable', 'off']
 
 class ServicesCmd(Component):
     """Telnet command to handle services."""
@@ -16,6 +20,27 @@ class ServicesCmd(Component):
     def executeCommand(self, args):
         services = service.IServiceCollection(self.env.app)
         data = []
+        if len(args)==3:
+            srv_name = args[2].lower()
+            action = args[1].lower()
+            if action in ENABLE:
+                self._enableService(srv_name)
+            elif action in DISABLE:
+                self._disableService(srv_name)
         for s in services:
             data.append(s.name + ' ' + str(s.running))
         return data
+    
+    @defer.inlineCallbacks
+    def _enableService(self, srv_name):
+        for srv in service.IServiceCollection(self.env.app):
+            if srv.name.lower()==srv_name.lower():
+                yield defer.maybeDeferred(srv.startService)
+                self.log.info('Stopping service %s' % srv.name)
+    
+    @defer.inlineCallbacks
+    def _disableService(self, srv_name):
+        for srv in service.IServiceCollection(self.env.app):
+            if srv.name.lower()==srv_name.lower():
+                yield defer.maybeDeferred(srv.stopService)
+                self.log.info('Starting service %s' % srv.name)    
