4ad4ce6101eccb42b7d9f0c7a9974dfa9e910c3e
Paul Käufl <p.j.kaufl@uu.nl>
2008-02-19 16:33:11 +0000

    changed some table definitions

--- xmldb/defaults.py
+++ xmldb/defaults.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from sqlalchemy import MetaData, Table, Column, Integer, String, \
-                       UniqueConstraint, ForeignKey
-
+                       UniqueConstraint, ForeignKey, Text, Binary, \
+                       PrimaryKeyConstraint, DateTime
 DEFAULT_PREFIX = 'default_'
 RESOURCE_TABLE = 'data'
 INDEX_TABLE = 'index'
 INDEX_DEF_TABLE = 'index_def'
-#METADATA_TABLE = 'meta'
+METADATA_TABLE = 'meta'
 #METADATA_INDEX_TABLE = 'meta_idx'
 URI_TABLE = 'uri'
 
@@ -15,21 +15,30 @@ URI_TABLE = 'uri'
 metadata = MetaData()
 resource_tab = Table(DEFAULT_PREFIX + RESOURCE_TABLE, metadata,
     Column('id', Integer, primary_key = True, autoincrement = True),
-    Column('data', String),
+    Column('data', Binary),
 )
 
 uri_tab = Table(DEFAULT_PREFIX + URI_TABLE, metadata,
-    Column('uri', String, primary_key = True),
+    Column('uri', Text),
+    Column('revision', Integer),
     Column('res_id', Integer, ForeignKey(DEFAULT_PREFIX + RESOURCE_TABLE +
                                          '.id')),
-    Column('res_type', String),
+    Column('res_type', Text),
+    PrimaryKeyConstraint('uri','revision')
+)
+
+metadata_tab = Table(DEFAULT_PREFIX + METADATA_TABLE, metadata,
+                     Column('res_id', Integer, 
+                            ForeignKey(DEFAULT_PREFIX + RESOURCE_TABLE + '.id')),
+                     Column('user', Text),
+                     Column('timestamp', DateTime)
 )
 
 # xmlindexcatalog tables:
 index_def_tab = Table(DEFAULT_PREFIX + INDEX_DEF_TABLE, metadata,
     Column('id', Integer, primary_key = True, autoincrement = True),
-    Column('key_path', String),
-    Column('value_path', String),
+    Column('key_path', Text),
+    Column('value_path', Text),
     Column('data_type', String(20)),
     UniqueConstraint('key_path', 'value_path')
 )
@@ -38,6 +47,6 @@ index_tab = Table(DEFAULT_PREFIX + INDEX_TABLE, metadata,
     Column('id', Integer, primary_key = True, autoincrement = True),
     Column('index_id', Integer, ForeignKey(DEFAULT_PREFIX + INDEX_DEF_TABLE +
                                            '.id')),
-    Column('key', String),
-    Column('value', String),
+    Column('key', Text),
+    Column('value', Text),
 )
