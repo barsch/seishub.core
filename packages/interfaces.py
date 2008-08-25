05d84ae5b562c48619de8669fb4222d2f11b840b
Robert Barsch <mail@robert-barsch.de>
2008-08-25 12:30:02 +0000

    - Mapper interfaces changed

--- packages/interfaces.py
+++ packages/interfaces.py
@@ -5,11 +5,11 @@ from zope.interface import Attribute
 
 
 class IPackage(Interface):
-    """This is the main interface for a unique seishub package."""
+    """This is the main interface for a unique SeisHub package."""
     
     package_id = Attribute("""
         Defines the package ID of this package.
- 
+        
         Single string representing a unique package id.
         """)
     
@@ -18,20 +18,20 @@ class IPackage(Interface):
         
         Version may be any string.
     """)
-    
-    
+
+
 class IPackageWrapper(IPackage):
     """Interface definition for a PackageWrapper class.
     
     A PackageWrapper is returned by the registry whenever a Package isn't present
     in the file system anymore but only in the database."""
-    
+
 
 class IResourceType(IPackage):
     """Interface definition for a unique resource type of a package."""
     
     resourcetype_id = Attribute("Defines the ID of this resource type.")
-    
+
 
 class IResourceTypeWrapper(IResourceType):
     """Interface definition for a ResourceTypeWrapper class.
@@ -40,15 +40,17 @@ class IResourceTypeWrapper(IResourceType):
     isn't present in the file system anymore but only in the database."""
 
 
-class IResourceMapper(IResourceType):
-    """Interface definition for a resource mapper for SFTP and REST."""
-    
-    def getMappingURL():
-        """
-        Define an user mapped URL.
+class IMapper(Interface):
+    """General interface definition for a resource mapper for SFTP and REST."""
+    mapping_url = Attribute("""
+        Defines the absolute URL of this mapping.
         
-        The full URL consists of package_id/resourcetype_id/mapping_url. 
-        """
+        Single string representing a unique mapping URL.
+        """)
+
+
+class IGETMapper(IMapper):
+    """Interface definition for a GET resource mapper."""
     
     def processGET(request):
         """
@@ -56,21 +58,33 @@ class IResourceMapper(IResourceType):
         
         This function should return a string containing a valid XML document.
         """
+
+
+class IPUTMapper(IMapper):
+    """Interface definition for a PUT resource mapper."""
     
     def processPUT(request):
         """
         Process a PUT request.
         
-        This function should return a string containing the new resource url.
+        This function should return a string containing the new resource URL.
         """
+
+
+class IPOSTMapper(IMapper):
+    """Interface definition for a POST resource mapper."""
     
     def processPOST(request):
         """
         Process a POST request.
         
-        This function should return a string containing the new resource url if
+        This function should return a string containing the new resource URL if
         the resource could be updated, otherwise a SeisHubError instance.
         """
+
+
+class IDELETEMapper(IMapper):
+    """Interface definition for a DELETE resource mapper."""
     
     def processDELETE(request):
         """
