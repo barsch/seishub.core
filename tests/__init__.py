565a2dc034b37d53d9269cc7fa3598efc858d497
Robert Barsch <mail@robert-barsch.de>
2008-09-14 19:28:03 +0000

    - seishub.core is also compatible to twisted.python - added a few testcases and a very well known doctest ;)

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,13 +4,16 @@ import doctest
 import unittest
 
 from seishub.tests import test_config, test_core, test_xmlconformance, \
-                          test_core_zope_compatibility
+                          test_core_zope_compatibility, \
+                          test_core_twisted_compatibility
 
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(test_core.suite())
     suite.addTest(test_core_zope_compatibility.suite())
+    suite.addTest(test_core_twisted_compatibility.suite())
+    suite.addTest(doctest.DocFileSuite('test_core_twisted_compatibility.txt'))
     suite.addTest(test_config.suite())
     suite.addTest(doctest.DocFileSuite('test_config.txt'))
     # XXX: add later
