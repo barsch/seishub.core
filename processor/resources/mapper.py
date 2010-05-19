b6d5e98f2df0b916d0618fb96524f4487b3088a2
Robert Barsch <mail@robert-barsch.de>
2010-05-19 10:28:46 +0000

    - allow public resources for mapper

--- processor/resources/mapper.py
+++ processor/resources/mapper.py
@@ -16,6 +16,8 @@ class MapperResource(Resource):
         Resource.__init__(self, **kwargs)
         self.is_leaf = True
         self.mapper = mapper
+        if hasattr(mapper, 'public'):
+            self.public = True
         if folderish:
             self.folderish = True
             self.category = 'mapping-folder'
