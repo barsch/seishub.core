660b9076e6c7f6344cfc85da72931cd907d89645
trichter <tom.eulenfeld@bgr.de>
2016-04-12 14:26:27 +0200

    add support for obspy 1.0

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
         'pycrypto',
         'pyasn1',
         'pyparsing',
-        'obspy<0.10.0',
+        'obspy<1.1',
     ],
     download_url="https://github.com/barsch/seishub.core/zipball/master" + \
         "#egg=seishub.core-dev",
